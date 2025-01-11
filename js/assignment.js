b02ad6450342c53479b760c69cfaf2f78108c2f5
adwenpa <joojo.abr@gmail.com>
2025-01-11 18:39:41 -0500

    made the required changes in assignment.js

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,24 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+function returnTheRemainder(num1, num2) {
+    return num1 % num2;
+}
+
+function checkIfEven(num) {
+    return (num % 2 === 0);
+}
+
+function getTheFourthElement(array1) {
+    if (array1.length >= 4) {
+        return array1[3];
+    }
+}
+
+submissionBtn.addEventListener("click", function() {
+    remainderElement.innerHTML = returnTheRemainder(5, 4);
+    checkIfEvenElement.innerHTML = checkIfEven(47);
+    lostNumbersElement.innerHTML = getTheFourthElement(lostNumbers);
+})
+
