abadde60d0cf90e5928ba3aa092c1e345ccf3077
adwenpa <joojo.abr@gmail.com>
2025-01-11 17:31:49 -0500

    Tried functions in  index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,32 @@
 // that are not declared
 "use strict"
 
+// const factorial = function fac(n) {
+//     return n < 2 ? 1 : n * fac(n - 1);
+//   };
+  
+//   console.log(factorial(3));
+
+//   function map(f, a) {
+//     const result = new Array(a.length);
+//     for (let i = 0; i < a.length; i++) {
+//       result[i] = f(a[i]);
+//     }
+//     return result;
+//   }
+  
+//   const cube = function (x) {
+//     return x * x * x;
+//   };
+  
+//   const numbers = [0, 1, 2, 5, 10];
+//   console.log(map(cube, numbers)); 
+
+//   function addSquares(a, b) {
+//     function square(x) {
+//       return x * x;
+//     }
+//     return square(a) + square(b);
+//   }
+  
+//   console.log(addSquares(2, 3));
